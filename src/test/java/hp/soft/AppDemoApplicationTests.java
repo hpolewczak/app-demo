9351f3308bb950db1d2abd0879b894e4ea5519be
Hadrian Polewczak <hadrian.polewczak@gmail.com>
2026-04-03 13:06:11 +0200

    initial commit with spring boot

---
+++ src/test/java/hp/soft/AppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hp.soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
