6bf8943566982753bf987f81c0d00c78ae30e165
Hadrian Polewczak <hadrian.polewczak@gmail.com>
2026-04-04 10:10:01 +0200

    Jooq config with support for Spring @Transactional

---
+++ src/main/java/hp/soft/config/JooqConfig.java
@@ -0,0 +1,30 @@
+package hp.soft.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.r2dbc.connection.TransactionAwareConnectionFactoryProxy;
+import org.springframework.transaction.ReactiveTransactionManager;
+
+@Configuration
+public class JooqConfig {
+
+    @Bean
+    public DSLContext dslContext(ConnectionFactory connectionFactory) {
+        return DSL.using(
+                new TransactionAwareConnectionFactoryProxy(connectionFactory),
+                SQLDialect.POSTGRES
+        );
+    }
+
+    @Bean
+    @Primary
+    public ReactiveTransactionManager reactiveTransactionManager(ConnectionFactory connectionFactory) {
+        return new R2dbcTransactionManager(connectionFactory);
+    }
+}
